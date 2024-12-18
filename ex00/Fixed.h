15fc2207736aa79b4ea976491415ca847b93e5bf
Illusimple <47168462+Abduux@users.noreply.github.com>
2024-12-18 09:15:46 +0000

    first commit

--- ex00/Fixed.h
+++ ex00/Fixed.h
@@ -31,30 +31,39 @@ class Fixed
         void setRawBits( int const raw );
 };
 
-Fixed::Fixed(const Fixed &other) // copy constructor 
+Fixed::Fixed(const Fixed &other) { // copy constructor 
+    std::cout << "Copy constructor called" << std::endl;
+    this->value = other.getRawBits();
+} 
+
+Fixed& Fixed::operator=(const Fixed& other) // copy assignment operator  
 {
-    this->value = other.value;
+    std::cout << "Copy assignment operator called " << std::endl;
+    if(this == &other)
+        return (*this);
+
+    this->value = other.getRawBits();
+    return *this;
 }
 
-Fixed& Fixed::operator=() // copy assignment operator  
-
-Fixed::Fixed(/* args */) : value(0)
-{
+Fixed::Fixed(/* args */) : value(0){
+    std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::~Fixed()
 {
+    std::cout << "Destructor Called !" << std::endl;
 }
 
 int Fixed::getRawBits() const 
 {
-    
+    std::cout << "getRawBits member function called" << std::endl;
+    return value;
 }
 
 void Fixed::setRawBits(int const raw)
 {
-    
+    this->value = raw;
 }
-
 
 #endif
